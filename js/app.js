4a903ba83d12cb63cec0d3ed9ed45e1f15fbe23e
Lyndi Holzwarth <lyndi.holzwarth@gmail.com>
2015-04-22 21:02:25 -0400

    cleaned up code

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-//ew game function
+//new game function
 	function newGame(){
 		numRand=Math.floor((Math.random() * 100)+1);
 		console.log("Answer " + numRand);
@@ -22,20 +22,18 @@ $(document).ready(function(){
     	$('#userGuess').attr("placeholder", "Enter your Guess").val("");
 	};
 
-
-newGame();
+//new game on initial load
+	newGame();
 
 //new game on button press
 	$('.new').click(function(){
 		newGame()
 	});
 
-
 //accept user guess
 	$( "form" ).on( 'click', '#guessButton', function( event ) {
-//		numRand;
-		var numGuess = $('#userGuess').val();	
 		event.preventDefault();
+		var numGuess = $('#userGuess').val();	
 		console.log(numGuess);
 
 //add guesses to list
@@ -48,69 +46,62 @@ newGame();
 
 //evaluate user guess for closeness and give message
 		var evalGuess = false;
-
 		while(!evalGuess){
-//test for real number
-		if (isNaN(numGuess)==true){
-			alert("Sorry, I need a real number. Please try again.");
-			$('#userGuess').attr("placeholder", "Enter your Guess").val("");	
-			break;
-			//maxNumber= false;
-		}
+			//test for real number
+			if (isNaN(numGuess)==true){
+				alert("Sorry, I need a real number. Please try again.");
+				$('#userGuess').attr("placeholder", "Enter your Guess").val("");	
+				break;
+			}
 
-		//test for decimal
-		else if (numGuess % 1 != 0){
-			alert("Sorry, I need a whole number. Please try again.");
-			$('#userGuess').attr("placeholder", "Enter your Guess").val("");	
-			break;
-		}
-
+			//test for decimal
+			else if (numGuess % 1 != 0){
+				alert("Sorry, I need a whole number. Please try again.");
+				$('#userGuess').attr("placeholder", "Enter your Guess").val("");	
+				break;
+			}
+			//test for enmpy field
+			else if (numGuess === ""){
+				alert("Sorry, I didn't catch that. Please try again.");
+				$('#userGuess').attr("placeholder", "Enter your Guess").val("");	
+				break;
+			}
 
 //continue if real number
 			else if (numGuess==numRand){
 		   		console.log("You Win");
 		   		evalGuess=true;
-		   //		alert("You win!");
+		   		alert("You win!");
 		   		$('#userGuess').val("You Win!");	
 		   		break;
 			}
 		   	else if (numGuess-numRand>=50||numRand-numGuess>=50){
 		   		evalGuess=false;
-		   	//	alert("Too Low. Guess again");
 			   $('#userGuess').attr("placeholder", "Freezing!").val("");	
 			   break;
 			}
 			else if (numGuess-numRand>=30||numRand-numGuess>=30){
 		   		evalGuess=false;
-		//   		alert("Too high. Guess again");
 			   $('#userGuess').attr("placeholder", "Cold").val("");	
 			   break;
 			}
 			else if (numGuess-numRand>=15||numRand-numGuess>=15){
 		   		evalGuess=false;
-		//   		alert("Too high. Guess again");
 			   $('#userGuess').attr("placeholder", "Warmer").val("");	
 			   break;
 			}
 			else if (numGuess-numRand>5||numRand-numGuess>5){
 		   		evalGuess=false;
-		//   		alert("Too high. Guess again");
 			   $('#userGuess').attr("placeholder", "Hot").val("");	
 			   break
 			  }
 			else if (numGuess-numRand<=5||numRand-numGuess<=5){
 		   		evalGuess=false;
-		//   		alert("Too high. Guess again");
 			   $('#userGuess').attr("placeholder", "Burning!").val("");	
 			   break;
 			}
 		} 
 	});
-
-
-
-//congrats message
-
 
 });
 
